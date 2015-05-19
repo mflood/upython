371ea73443fbfc1b84becafa19d6a116f670b5bf
Matthew Flood <mflood@shutterstock.com>
2015-05-18 20:00:01 -0600

    more methods

--- main.py
+++ main.py
@@ -15,6 +15,28 @@ class SimpleWriter(object):
     def __repr__(self): 
         return "SimpleWriter %s" % self.output_file
 
+class NumberedWriter(object):
+    # add line numbers before each line
+
+class BufferedWriter(object):
+    # only writes out when it reaches N data messages
+
+class SkipWriter(object):
+    
+    def __init__(self, output_file, number_to_skip):
+        self.number_to_skip  = number_to_skip
+        self.output_file = output_file
+        self.num_skipped = 0
+
+    def write(self, data):
+        if self.num_skipped < self.number_to_skip:
+            self.num_skipped += 1
+            return
+
+        self.num_skipped = 0
+        with open(self.output_file, "a") as out_handle:
+            out_handle.write(data)
+
 class DummyWriter(object):
 
     def write(self, data):
@@ -34,7 +56,8 @@ def main():
     
     print "Input file: %s  Output file: %s" % (input_file, output_file)
 
-    my_writer = SimpleWriter(output_file)
+    my_writer = SkipWriter(output_file, 4)
+    #my_writer = SimpleWriter(output_file)
 
     #my_writer = DummyWriter()
 
