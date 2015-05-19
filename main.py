d9c84f722dba8c7e8195b4c04ce7ce16e516f86f
Matthew Flood <mflood@shutterstock.com>
2015-05-18 19:45:41 -0600

    SimpleWriter

---
+++ main.py
@@ -0,0 +1,35 @@
+
+
+
+import sys
+
+class SimpleWriter(object):
+    
+    def __init__(self, output_file):
+        self.output_file = output_file
+
+    def write(self, data):
+        with open(self.output_file, "a") as out_handle:
+            out_handle.write(data)
+
+    def __repr__(self): 
+        return "SimpleWriter %s" % self.output_file
+
+def main():
+    input_args = sys.argv;
+    if len(input_args) != 3:
+        print "Usage: python main.py <inputfile> <outputfile>"
+        sys.exit(-1)
+
+    input_file = input_args[1]
+    output_file = input_args[2]
+    
+    print "Input file: %s  Output file: %s" % (input_file, output_file)
+
+    my_writer = SimpleWriter(output_file)
+
+    with open(input_file, "r") as in_handle:
+        for line in in_handle:
+            my_writer.write(line)
+
+main()
